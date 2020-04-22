17b6ca48f31d28f107a5c8faf7ae306daeb828b1
Antoine <Antoine.noe.dev@gmail.com>
2020-04-22 14:48:50 +0200

    Rectif form avec labels

--- emailThis.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$_POST['name'];
-$_POST['email'];
-$_POST['message'];
-
-mail( "Antoine.noe.pro@gmail.com",  "AlphaMuppets",  "Hey ".$_POST['name'].", tout fonctionne ici !"." Merci pour ton message : \"".$_POST['message']."\"");
